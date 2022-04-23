c3469558e08d1a35046b83c04da4214f7ea7930d
Brenden Thornton <brenden@bowtie.co>
2022-04-23 13:17:55 -0600

    created login form, worked out a decent pattern for page layout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45ff367b2696e13b"; }
+/******/ 		__webpack_require__.h = function() { return "663607264c0570d4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
